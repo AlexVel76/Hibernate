63d7a3c98ea5e92cc4f3b50b174652ac2b36f624
Oleksandr Velchenko <oleksandr_velchenko@epam.com>
2018-10-26 16:12:26 +0300

    create spring boot template

---
+++ nPlusOneDesicions/src/main/java/com/hibernate/study/nPlusOneDesicions/NPlusOneDesicionsApplication.java
@@ -0,0 +1,12 @@
+package com.hibernate.study.nPlusOneDesicions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NPlusOneDesicionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NPlusOneDesicionsApplication.class, args);
+	}
+}
